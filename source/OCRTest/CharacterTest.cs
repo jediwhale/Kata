af52e824694e7164aa98f50087eb59dc015301a9
Mike Stockdale <mikes@syterra.com>
2017-04-23 15:30:27 -0600

    add nunit

---
+++ source/OCRTest/CharacterTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Syterra.Kata.OCRTest {
+    [TestFixture]
+    public class CharacterTest {
+
+        [Test]
+        public void Dummy() {
+            Assert.IsTrue(true);
+        }
+        
+    }
+}
