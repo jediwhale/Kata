d05fdf71765f530e6ad21d5ed6c36fa255dc90be
Mike Stockdale <mikes@syterra.com>
2017-04-23 15:16:14 -0600

    initial commit

---
+++ source/OCR/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OCR")]
+[assembly: AssemblyDescription("OCR kata")]
+[assembly: AssemblyCompany("Syterra Software Inc.")]
+[assembly: AssemblyProduct("Kata")]
+[assembly: AssemblyCopyright("Copyright © Syterra Software Inc. 2017")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]
---
+++ source/OCRTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OCRTest")]
+[assembly: AssemblyDescription("Tests for OCR kata")]
+[assembly: AssemblyCompany("Syterra Software Inc.")]
+[assembly: AssemblyProduct("Kata")]
+[assembly: AssemblyCopyright("Copyright © Syterra Software Inc. 2017")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]
