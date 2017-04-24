364ad15030bd431ff0ee9433a52efd1b32557995
Mike Stockdale <mikes@syterra.com>
2017-04-24 16:45:25 -0600

    validate checksum

---
+++ source/OCR/Checksum.cs
@@ -0,0 +1,14 @@
+﻿namespace Syterra.Kata.OCR {
+    public class Checksum {
+        public static object IsValid(string input) {
+            var sum = 0;
+            var count = 9;
+            foreach (var digit in input) {
+                sum += (digit - '0') * count;
+                if (count == 1) break;
+                count--;
+            }
+            return sum % 11 == 0;
+        }
+    }
+}
---
+++ source/OCRTest/ChecksumTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Syterra.Kata.OCR;
+
+namespace Syterra.Kata.OCRTest {
+    [TestFixture]
+    public class ChecksumTest {
+
+        [TestCase("457508000", true)]
+        [TestCase("664371495", false)]
+        public void ValidatesChecksum(string input, bool expected) {
+            Assert.AreEqual(expected, Checksum.IsValid(input));
+        }
+        
+    }
+}
