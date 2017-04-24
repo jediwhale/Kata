f54ea87c1e241c0d5e30f968f2a22cbca7529fc7
Mike Stockdale <mikes@syterra.com>
2017-04-24 17:34:19 -0600

    main program

---
+++ source/OCRProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Syterra.Kata.OCR;
+
+namespace Syterra.Kata.OCRProgram {
+    class Program {
+        static void Main(string[] args) {
+            foreach (var output in Parser.Parse(File.ReadAllLines(args[0])).Select(Report.Write)) {
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
---
+++ source/OCRProgram/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OCRProgram")]
+[assembly: AssemblyDescription("OCR kata main program")]
+[assembly: AssemblyCompany("Syterra Software Inc.")]
+[assembly: AssemblyProduct("Kata")]
+[assembly: AssemblyCopyright("Copyright © Syterra Software Inc. 2017")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]
