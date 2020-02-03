be7c84740598de774ac62d68d0bac2a81d622e99
Mike Stockdale <mikes@syterra.com>
2020-02-02 20:57:12 -0700

    rook moves

--- KingKata/KingTest/Test.cs
+++ KingKata/KingTest/Test.cs
@@ -7,16 +7,6 @@ namespace KingTest {
     public class ChessBoard {
         public const int Size = 8;
         
-        public static IEnumerable<Square> Squares {
-            get {
-                for (var row = 0; row < Size; row++) {
-                    for (var col = 0; col < Size; col++) {
-                        yield return Square.Make(row, col);
-                    }
-                }
-            }
-        }
-
         public ChessBoard(char[,] contents) {
             for (var row = 0; row < Size; row++) {
                 for (var col = 0; col < Size; col++) {
@@ -104,7 +94,8 @@ namespace KingTest {
             = new Dictionary<char, Func<IEnumerable<Func<Square, IEnumerable<Square>>>>> {
                 {'B', BishopAttacks},
                 {'K', KingAttacks},
-                {'P', PawnAttacks}
+                {'P', PawnAttacks},
+                {'R', RookAttacks}
             };
         
         static IEnumerable<Func<Square, IEnumerable<Square>>> BishopAttacks() {
@@ -112,6 +103,29 @@ namespace KingTest {
             yield return DiagonalUpRight;
             yield return DiagonalDownLeft;
             yield return DiagonalDownRight;
+        }
+        
+        static IEnumerable<Func<Square, IEnumerable<Square>>> RookAttacks() {
+            yield return StraightUp;
+            yield return StraightLeft;
+            yield return StraightRight;
+            yield return StraightDown;
+        }
+        
+        static IEnumerable<Square> StraightUp(Square location) {
+            return MultipleMoves(location, Movement.Up);
+        }
+        
+        static IEnumerable<Square> StraightLeft(Square location) {
+            return MultipleMoves(location, Movement.Left);
+        }
+        
+        static IEnumerable<Square> StraightRight(Square location) {
+            return MultipleMoves(location, Movement.Right);
+        }
+        
+        static IEnumerable<Square> StraightDown(Square location) {
+            return MultipleMoves(location, Movement.Down);
         }
         
         static IEnumerable<Square> DiagonalUpLeft(Square location) {
@@ -186,6 +200,19 @@ namespace KingTest {
                 "        ",
                 "K       ");
         }
+        
+        [Test]
+        public void CheckByRook() {
+            AssertCheck(true, 
+                "        ",
+                "        ",
+                "        ",
+                "        ",
+                "  K  R  ",
+                "        ",
+                "        ",
+                "        ");
+        }
 
         void AssertCheck(bool expected, params string[] rows) {
             var squares = new char[8, 8];
@@ -199,13 +226,6 @@ namespace KingTest {
     
     [TestFixture]
     public class Test {
-        [Test]
-        public void SquaresAreEnumerated() {
-            var squares = ChessBoard.Squares.ToList();
-            Assert.AreEqual(64, squares.Count);
-            Assert.AreEqual(Square.Make(0,0), squares[0]);
-            Assert.AreEqual(Square.Make(7,7), squares[63]);
-        }
 
         [Test]
         public void LeftMovementIsCalculated() {
@@ -239,6 +259,15 @@ namespace KingTest {
             AssertCaptures(captures[1], Square.Make(2,4), Square.Make(1,5), Square.Make(0,6), Square.None, Square.None, Square.None, Square.None);
             AssertCaptures(captures[2], Square.Make(4,2), Square.Make(5,1), Square.Make(6,0), Square.None, Square.None, Square.None, Square.None);
             AssertCaptures(captures[3], Square.Make(4,4), Square.Make(5,5), Square.Make(6,6), Square.Make(7,7), Square.None, Square.None, Square.None);
+        }
+
+        [Test]
+        public void RookCapturesAreGenerated() {
+            var captures = Rules.PieceAttacks['R']().ToList();
+            AssertCaptures(captures[0], Square.Make(2,3), Square.Make(1,3), Square.Make(0,3), Square.None, Square.None, Square.None, Square.None);
+            AssertCaptures(captures[1], Square.Make(3,2), Square.Make(3,1), Square.Make(3,0), Square.None, Square.None, Square.None, Square.None);
+            AssertCaptures(captures[2], Square.Make(3,4), Square.Make(3,5), Square.Make(3,6), Square.Make(3,7), Square.None, Square.None, Square.None);
+            AssertCaptures(captures[3], Square.Make(4,3), Square.Make(5,3), Square.Make(6,3), Square.Make(7,3), Square.None, Square.None, Square.None);
         }
  
         static void AssertCaptures(Func<Square, IEnumerable<Square>> generator, params Square[] expected) {
